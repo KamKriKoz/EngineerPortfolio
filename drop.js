5a8b5d3214ce9b53a42bd420691efc9a347de30a
KamKriKoz <73202764+KamKriKoz@users.noreply.github.com>
2024-06-18 13:30:19 +0200

    Add files via upload

---
+++ drop.js
@@ -0,0 +1,11 @@
+function dropFunction() {
+    document.getElementById("dropDown").classList.toggle("show");
+}
+
+function dropFunction2() {
+    document.getElementById("dropDown2").classList.toggle("show");
+}
+
+function dropFunction3() {
+    document.getElementById("dropDown3").classList.toggle("show");
+}
